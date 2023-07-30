d4dc1f1fa6f8cdcf5315d627c46fdad8efbad7e7
jung9wo <wjddnalt24@gmail.com>
2023-07-30 13:11:48 +0900

    UserStorage 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const UserStorage = require("../../modles/UserStorage");
+
 const output = {
   home: (req, res) => {
     res.render("home/index");
@@ -9,15 +11,12 @@ const output = {
   },
 };
 
-const users = {
-  id: ["wjddnalt24", "나개발", "김팀장"],
-  psword: ["1234", "1234", "123456"],
-};
-
 const process = {
   login: (req, res) => {
     const id = req.body.id,
       psword = req.body.psword;
+    
+    const users = UserStorage.getUsers("id", "psword");
 
     const response = {}; 
     if (users.id.includes(id)) {
